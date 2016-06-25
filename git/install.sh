ef374feb3a52909d051d2a047950b8b788e1e15e
Fan Zhang <ffanzhang@hotmail.com>
2016-06-24 17:04:27 -0700

    added git merge settings

--- git/install.sh
+++ git/install.sh
@@ -2,4 +2,7 @@ if [ -x /bin/vim ]
 then
 	git config --global diff.tool vimdiff
 	git config --global difftool.prompt false
+	git config --global merge.tool vimdiff
+	git config --global mergetool.prompt false
+	git config --global merge.conflictstyle diff3
 fi
