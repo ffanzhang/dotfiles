c804763f15862b89b6c5b53a7a2d74a82b5d5347
Fan Zhang <ffanzhang@hotmail.com>
2016-07-04 11:14:43 -0700

    added more vim airline settings

--- vim/install.sh
+++ vim/install.sh
@@ -6,18 +6,18 @@ command_exists () {
 
 if command_exists vim
 then
-	if ! [ -d ~/.vim/bundle/Vundle.vim ]
-	then
-		sudo git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	fi
+        if ! [ -d ~/.vim/bundle/Vundle.vim ]
+        then
+                sudo git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        fi
 
-	pushd ~/.vim/bundle/Vundle.vim
+        pushd ~/.vim/bundle/Vundle.vim
         sudo git stash
-	sudo submodule update --init --recursive
-	popd
+        sudo submodule update --init --recursive
+        popd
 
         sudo cp vimrc ~/.vimrc
-	sudo vim +PluginInstall +qall
+        sudo vim +PluginInstall +qall
 
         if command_exists apt-get
         then
@@ -32,18 +32,20 @@ then
                         sudo apt-get install llvm
                 fi
         fi
-        
-	if command_exists pacman && command_exists yaourt
-	then
-                sudo pacman -S xsel
-		ycm_string=$(yaourt -Qm | grep youcompleteme)
-		if [ ${#ycm_string} -eq 0 ]
-		then
-			echo Installing youcompleteme using yaourt
-			gpg --recv-keys F7E48EDB &
-			yaourt -S ncurses5-compat-libs &
-			yaourt -S vim-youcompleteme-git &
-		fi
+
+        if command_exists pacman && command_exists yaourt
+        then
+                if ! command_exists xsel; then
+                        sudo pacman -S xsel
+                fi
+                ycm_string=$(yaourt -Qm | grep youcompleteme)
+                if [ ${#ycm_string} -eq 0 ]
+                then
+                        echo Installing youcompleteme using yaourt
+                        gpg --recv-keys F7E48EDB &
+                        yaourt -S ncurses5-compat-libs &
+                        yaourt -S vim-youcompleteme-git &
+                fi
 
                 if ! command_exists latex
                 then
@@ -53,27 +55,27 @@ then
                 echo Installing using another method
                 # attempt to manually compile youcompleteme in background
                 # sudo ~/.vim/bundle/YouCompleteMe/install.py --all &
-	fi
+        fi
 
-	ycm_config_pathname=~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
-	if ! [ -f $ycm_config_pathname ]
-	then
-		echo Downloading C/C++ config file for youcompleteme
-		sudo wget https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py -P ~/.vim/bundle/YouCompleteMe
-	fi
+        ycm_config_pathname=~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
+        if ! [ -f $ycm_config_pathname ]
+        then
+                echo Downloading C/C++ config file for youcompleteme
+                sudo wget https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py -P ~/.vim/bundle/YouCompleteMe
+        fi
 
-	# install tern for javascript
-	if [ -d ~/.vim/bundle/tern_for_vim ]
-	then
-		pushd ~/.vim/bundle/tern_for_vim
-		if ! [ -d node_modules ] && command_exists npm
-		then
-			echo Installing tern
-			sudo npm install
-		fi
-		popd
-	fi
+        # install tern for javascript
+        if [ -d ~/.vim/bundle/tern_for_vim ]
+        then
+                pushd ~/.vim/bundle/tern_for_vim
+                if ! [ -d node_modules ] && command_exists npm
+                then
+                        echo Installing tern
+                        sudo npm install
+                fi
+                popd
+        fi
 
-	# javascript autocompletion config file
-	cp tern-project ~/.tern-project
+        # javascript autocompletion config file
+        cp tern-project ~/.tern-project
 fi
