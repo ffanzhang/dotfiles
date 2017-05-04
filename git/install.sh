b4fe209fdb6f78fb72bcb2c478dfc888fc07690d
Fan Zhang <ffanzhang@hotmail.com>
2017-05-03 20:48:29 -0700

    improved logic for git settings

--- git/install.sh
+++ git/install.sh
@@ -2,11 +2,15 @@ command_exists() {
         command -v "$1" > /dev/null
 }
 
-if command_exists vim && command_exists git
+if command_exists git
+then
+	git config --global difftool.prompt false
+	git config --global merge.conflictstyle diff3
+	git config --global mergetool.prompt false
+fi
+
+if command_exists git && command_exists vim
 then
 	git config --global diff.tool vimdiff
-	git config --global difftool.prompt false
 	git config --global merge.tool vimdiff
-	git config --global mergetool.prompt false
-	git config --global merge.conflictstyle diff3
 fi
