e2a3fb63a7a63611f3d21d307430c96e951ac04d
Fan Zhang <ffanzhang@hotmail.com>
2017-05-03 20:42:50 -0700

    added more unix options installing vim and git

--- install.sh
+++ install.sh
@@ -1,3 +1,38 @@
+command_exists() {
+  command -v "$1" > /dev/null
+}
+
+if command_exists yum
+then
+  sudo yum -y update
+  sudo yum -y install vim
+  sudo yum -y install git
+fi
+
+if command_exists apt-get
+then
+  sudo apt-get update
+  sudo apt-get install vim
+  sudo apt-get install git
+fi
+
+if command_exists pacman
+then
+  sudo pacman -Syu
+  sudo pacman -S vim
+  sudo pacman -S git
+fi
+
+if command_exists brew
+then
+  brew update
+  brew install vim
+  brew install macvim
+  brew link macvim
+  brew install git
+  brew install gcc-6
+fi
+
 for folder in `find * -type d`
 do
 	pushd $folder
