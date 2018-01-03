5858c9ca5a1068c64a4540ff28377c5c476d7e90
ffanzhang <ffanzhang@hotmail.com>
2018-01-02 22:18:26 -0800

    Xresources tweaking

--- Xresources/install.sh
+++ Xresources/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ../utils.sh
 install_with_bak Xresources ~/.Xresources
-xrdb -merge ~/.Xresources
+xrdb -load ~/.Xresources
--- install_simple.sh
+++ install_simple.sh
@@ -7,5 +7,5 @@ install_with_bak vim/tern-project ~/.tern-project
 install_with_bak Xresources/Xresources ~/.Xresources
 install_with_bak tmux/tmux.conf ~/.tmux.conf
 
-xrdb -merge ~/.Xresources
+xrdb -load ~/.Xresources
 echo done with basic setup
