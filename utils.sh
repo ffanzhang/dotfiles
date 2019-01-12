ad09a90e94e4b4da031c4745abec582c05b02d35
ffanzhang <ffanzhang@hotmail.com>
2019-01-12 10:02:34 -0800

    change date format for baks

--- utils.sh
+++ utils.sh
@@ -6,13 +6,10 @@ command_exists() {
 # $1: source file directory
 # $2: dest file directory
 install_with_bak() {
-    if [ -f $2 ]
+    # only make a backup if the new file is different
+    if [ -f $2 ] && ! [ "$(diff $1 $2)" == "" ]
     then
-        # only make a backup if the new file is different
-        if [ "$(diff $1 $2)" != "" ]
-        then
-            mv $2 $2.bak.$(date +%s%3N)
-        fi
-    fi
+        mv $2 $2.bak.$(date +%Y%m%d-%H-%M-%S).$(date +%s%3N)
+    fi 
     cp $1 $2
 }
